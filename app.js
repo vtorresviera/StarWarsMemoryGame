75be01367e7c7aed48895368f44518a2f8dad27d
Dan Macre <dan@Dans-MacBook-Pro.local>
2015-05-13 12:01:03 -0400

    5.12

--- app.js
+++ app.js
@@ -1,90 +1,73 @@
-/*---EXAMPLE-----
+<script>newboard();</script>
 
-var Tile = function(x, y, face) {
-    this.x = x;
-    this.y = y;
-    this.face = face;
-    this.width = 70;
-};
-
-Tile.prototype.drawFaceDown = function() {
-    fill(214, 247, 202);
-    strokeWeight(2);
-    rect(this.x, this.y, this.width, this.width, 10);
-    image(getImage("avatars/leaf-green"), this.x, this.y, this.width, this.width);
-};
-
-Tile.prototype.drawFaceUp = function() {
-    fill(214, 247, 202);
-    strokeWeight(2);
-    rect(this.x, this.y, this.width, this.width, 10);
-    image(this.face, this.x, this.y, this.width, this.width);
-};
-
-Tile.prototype.isUnderMouse = function(x, y) {
-    return x >= this.x && x <= this.x + this.width  &&
-        y >= this.y && y <= this.y + this.width;
-};
-
-// Declare an array of all possible faces
-var faces = [
-    getImage("avatars/leafers-seed"),
-    getImage("avatars/leafers-seedling"),
-    getImage("avatars/leafers-sapling"),
-    getImage("avatars/leafers-tree"),
-    getImage("avatars/leafers-ultimate"),
-    getImage("avatars/marcimus"),
-    getImage("avatars/mr-pants"),
-    getImage("avatars/mr-pink"),
-    getImage("avatars/old-spice-man"),
-    getImage("avatars/robot_female_1")
-];
-
-// Make an array which has 2 of each, then randomize it
-var possibleFaces = faces.slice(0);
-var selected = [];
-for (var i = 0; i < 10; i++) {
-    // Randomly pick one from the array of remaining faces
-    var randomInd = floor(random(possibleFaces.length));
-    var face = possibleFaces[randomInd];
-    // Push 2 copies onto array
-    selected.push(face);
-    selected.push(face);
-    // Remove from array
-    possibleFaces.splice(randomInd, 1);
-}
-
-// Now we need to randomize the array
-selected.sort(function() {
-    return 0.5 - Math.random();
-});
-
-// Create the tiles
-var tiles = [];
-for (var i = 0; i < 5; i++) {
-    for (var j = 0; j < 4; j++) {
-        tiles.push(new Tile(i * 78 + 10, j * 78 + 40, selected.pop()));
+   <script>
+// Scripted By Adam Khoury in connection with the following video tutorial:
+// http://www.youtube.com/watch?v=c_ohDPWmsM0
+var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
+var memory_values = [];
+var memory_tile_ids = [];
+var tiles_flipped = 0;
+Array.prototype.memory_tile_shuffle = function(){
+    var i = this.length, j, temp;
+    while(--i > 0){
+        j = Math.floor(Math.random() * (i+1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
     }
 }
-
-background(255, 255, 255);
-
-// Now draw them face up
-for (var i = 0; i < tiles.length; i++) {
-    tiles[i].drawFaceDown();
+function newBoard(){
+    tiles_flipped = 0;
+    var output = '';
+    memory_array.memory_tile_shuffle();
+    for(var i = 0; i < memory_array.length; i++){
+        output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+    }
+    document.getElementById('memory_board').innerHTML = output;
 }
-
-mouseClicked = function() {
-    // check if mouse was inside a tile
-    for (var i = 0; i < tiles.length; i++) {
-        if (tiles[i].isUnderMouse(mouseX, mouseY)) {
-            tiles[i].drawFaceUp();
+function memoryFlipTile(tile,val){
+    if(tile.innerHTML == "" && memory_values.length < 2){
+        tile.style.background = '#FFF';
+        tile.innerHTML = val;
+        if(memory_values.length == 0){
+            memory_values.push(val);
+            memory_tile_ids.push(tile.id);
+        } else if(memory_values.length == 1){
+            memory_values.push(val);
+            memory_tile_ids.push(tile.id);
+            if(memory_values[0] == memory_values[1]){
+                tiles_flipped += 2;
+                // Clear both arrays
+                memory_values = [];
+                memory_tile_ids = [];
+                // Check to see if the whole board is cleared
+                if(tiles_flipped == memory_array.length){
+                    alert("Board cleared... generating new board");
+                    document.getElementById('memory_board').innerHTML = "";
+                    newBoard();
+                }
+            } else {
+                function flip2Back(){
+                    // Flip the 2 tiles back over
+                    var tile_1 = document.getElementById(memory_tile_ids[0]);
+                    var tile_2 = document.getElementById(memory_tile_ids[1]);
+                    tile_1.style.background = 'url(tile_bg.jpg) no-repeat';
+                    tile_1.innerHTML = "";
+                    tile_2.style.background = 'url(tile_bg.jpg) no-repeat';
+                    tile_2.innerHTML = "";
+                    // Clear both arrays
+                    memory_values = [];
+                    memory_tile_ids = [];
+                }
+                setTimeout(flip2Back, 700);
+            }
         }
     }
-};
-*/
+}
+window.AddEventListener(type,lisener,useCapture)('memory_board.innerHTML')=output;
+</script>
 
-/*--Timer function--*/
+
 <script>
 (function(){
 
