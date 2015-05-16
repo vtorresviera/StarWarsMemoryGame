47164aad021a0bb53172642ed1016e186c73c6e5
Vanessa <vtorresviera@gmail.com>
2015-05-16 19:32:41 -0400

    trouble-shooting with pictures

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -116,9 +116,9 @@ function memoryFlipTile(tile, val) {
                     // Flip the 2 tiles back over
                     var tile_1 = document.getElementById(memory_tile_ids[0]);
                     var tile_2 = document.getElementById(memory_tile_ids[1]);
-                    tile_1.style.background = 'url(../img/backcard.png) no-repeat';
+                    tile_1.style.background = 'url(img/backcard.png) no-repeat';
                     tile_1.innerHTML = "";
-                    tile_2.style.background = 'url(../img/backcard.png) no-repeat';
+                    tile_2.style.background = 'url(img/backcard.png) no-repeat';
                     tile_2.innerHTML = "";
                     //add to incorrect matches 
                     numIncorrectMatches += 1;
